428d706f21b163dce5b3b36db3e33ba92528958f
Lucas Thobias <lucasthobiasc@gmail.com>
2023-03-20 09:30:18 -0300

    Controle de Ponto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [hoursWorked, setHoursWorked] = useState(0);
+  const [isWorking, setIsWorking] = useState(false);
+
+  useEffect(() => {
+    const fetchHoursWorked = async () => {
+      const response = await axios.get('/api/hours-worked');
+      setHoursWorked(response.data.hoursWorked);
+    };
+
+    fetchHoursWorked();
+  }, []);
+
+  const handleStartWork = async () => {
+    await axios.post('/api/start-work');
+    setIsWorking(true);
+  };
+
+  const handleEndWork = async () => {
+    await axios.post('/api/end-work');
+    setIsWorking(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Controle de Ponto</h1>
+      <p>Horas trabalhadas hoje: {hoursWorked}</p>
+      {isWorking ? (
+        <button onClick={handleEndWork}>Finalizar turno</button>
+      ) : (
+        <button onClick={handleStartWork}>Iniciar turno</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [hoursWorked, setHoursWorked] = useState(0);
+  const [isWorking, setIsWorking] = useState(false);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    const fetchHoursWorked = async () => {
+      const response = await axios.get('/api/hours-worked');
+      setHoursWorked(response.data.hoursWorked);
+    };
+
+    fetchHoursWorked();
+  }, []);
+
+  const handleStartWork = async () => {
+    await axios.post('/api/start-work');
+    setIsWorking(true);
+  };
+
+  const handleEndWork = async () => {
+    await axios.post('/api/end-work');
+    setIsWorking(false);
+  };
+
+  return (
+    <div>
+      <h1>Controle de Ponto</h1>
+      <p>Horas trabalhadas hoje: {hoursWorked}</p>
+      {isWorking ? (
+        <button onClick={handleEndWork}>Finalizar turno</button>
+      ) : (
+        <button onClick={handleStartWork}>Iniciar turno</button>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
